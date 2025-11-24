2142a1bc500b16881970fb550b7f16b963f7bedb
Dave Kimura <dave@k-innovations.net>
2025-11-24 07:53:13 -0500

    (chore(action_auth)): bump version to 1.8.7 and update dependencies
      - Update ActionAuth gem to 1.8.7 and adjust version constant
      - Upgrade Rails and associated gems to 8.1.1 (e.g., actioncable, actionmailbox, etc.)
      - Update Gemfile.lock to reflect new gem versions and dependency changes
      - Strengthen test password values for passwords_controller tests to improve security testing

--- test/controllers/action_auth/passwords_controller_test.rb
+++ test/controllers/action_auth/passwords_controller_test.rb
@@ -14,12 +14,12 @@ module ActionAuth
     end
 
     test "should update password" do
-      patch password_path, params: { password_challenge: "123456789012", password: "NewPassword123456789", password_confirmation: "NewPassword123456789" }
+      patch password_path, params: { password_challenge: "123456789012", password: "MyV3ry$ecureP@ssw0rd!", password_confirmation: "MyV3ry$ecureP@ssw0rd!" }
       assert_response :redirect
     end
 
     test "should not update password with wrong password challenge" do
-      patch password_path, params: { password_challenge: "SecretWrong1*3", password: "NewPassword123456789", password_confirmation: "NewPassword123456789" }
+      patch password_path, params: { password_challenge: "SecretWrong1*3", password: "MyV3ry$ecureP@ssw0rd!", password_confirmation: "MyV3ry$ecureP@ssw0rd!" }
 
       assert_response :unprocessable_entity
       assert_select "li", /Password challenge is invalid/
